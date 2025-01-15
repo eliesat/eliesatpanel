b36dba715b97cafc8326983767d52df76546d5b3
eliesat <112230228+eliesat@users.noreply.github.com>
2025-01-15 18:06:59 +0200

    Add files via upload

---
+++ menus/imagesb.py
@@ -0,0 +1,200 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Plugins.Extensions.ElieSatPanel.menus.Console import Console
+import gettext
+from Components.Language import language
+from Components.PluginComponent import plugins
+from Components.Sources.StaticText import StaticText
+from Components.Pixmap import Pixmap
+from Components.ActionMap import ActionMap, NumberActionMap
+from Components.Sources.List import List
+import os
+from Plugins.Plugin import PluginDescriptor
+from Screens.Standby import TryQuitMainloop
+from Screens.MessageBox import MessageBox
+from Components.Console import Console as iConsole
+from Tools.Directories import fileExists, pathExists, resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
+from types import *
+from Components.Label import Label
+from Components.MenuList import MenuList
+from Screens.PluginBrowser import PluginBrowser
+from Screens.Screen import Screen
+from Tools.LoadPixmap import LoadPixmap
+
+global min, first_start
+min = first_start = 0
+####################################
+
+class imagesb(Screen):
+	skin = """
+<screen name="plugins" position="0,0" size="1920,1080" backgroundColor="transparent" flags="wfNoBorder" title="ElieSatPanel">
+<ePixmap position="0,0" zPosition="-1" size="1920,1080" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/ElieSatPanel/images/bglist.png"/>
+
+<!-- title -->
+<eLabel text="INSTALL" position="160,120" size="400,50" zPosition="1" font="Regular;45" halign="left" foregroundColor="white" backgroundColor="#ff2c2d2b" transparent="1" />
+<ePixmap position="55,125" size="180,47" zPosition="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/ElieSatPanel/images/1.png" alphatest="blend" />
+
+
+<!-- minitv -->
+<widget source="session.VideoPicture" render="Pig" position="1320,120" size="550,400" zPosition="1" backgroundColor="#ff000000" />
+
+<!-- clock -->
+<widget source="global.CurrentTime" render="Label" position="1310,600" size="350,90" font="lsat; 75" noWrap="1" halign="center" valign="bottom" foregroundColor="#11ffffff" backgroundColor="#20000000" transparent="1" zPosition="2">
+		<convert type="ClockToText">Default</convert>
+
+<!-- calender -->
+</widget>
+<widget source="global.CurrentTime" render="Label" position="1530,610" size="335,54" font="lsat; 24" halign="center" valign="bottom" foregroundColor="#11ffffff" backgroundColor="#20000000" transparent="1" zPosition="1">
+<convert type="ClockToText">Format %A %d %B</convert>
+</widget>
+
+<!-- button -->
+<ePixmap position="120,930" zPosition="1" size="240,10" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/ElieSatPanel/images/red.png" alphatest="blend" />
+<widget source="key_red" render="Label" position="160,870" zPosition="2" size="165,45" font="Regular;35" halign="center" valign="center" backgroundColor="background" foregroundColor="foreground" transparent="1" />
+<ePixmap position="400,930" zPosition="1" size="240,10" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/ElieSatPanel/images/green.png" alphatest="blend" />
+<widget source="key_green" render="Label" position="440,870" zPosition="2" size="165,45" font="Regular;35" halign="center" valign="center" backgroundColor="background" foregroundColor="foreground" transparent="1" />
+<ePixmap position="680,930" zPosition="1" size="240,10" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/ElieSatPanel/images/yellow.png" alphatest="blend" />
+<widget source="key_yellow" render="Label" position="720,870" zPosition="2" size="165,45" font="Regular;35" halign="center" valign="center" backgroundColor="background" foregroundColor="foreground" transparent="1" />
+<ePixmap position="960,930" zPosition="1" size="240,10" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/ElieSatPanel/images/blue.png" alphatest="blend" />
+<widget source="key_blue" render="Label" position="1000,870" zPosition="2" size="165,45" font="Regular;35" halign="center" valign="center" backgroundColor="background" foregroundColor="foreground" transparent="1" />
+
+<!-- menu list -->
+<widget source="menu" selectionPixmap="/usr/lib/enigma2/python/Plugins/Extensions/ElieSatPanel/images/selection.png" render="Listbox" position="48,200" size="1240,660" scrollbarMode="showOnDemand">
+<convert type="TemplatedMultiContent">
+	{"template": [
+		MultiContentEntryText(pos = (120, 10), size = (600, 45), font=0, flags = RT_HALIGN_LEFT, text = 0), # index 2 is the Menu Titel
+		MultiContentEntryText(pos = (700, 19), size = (600, 35), font=1, flags = RT_HALIGN_LEFT, text = 2), # index 3 is the Description
+		MultiContentEntryPixmapAlphaTest(pos = (6, 10), size = (100, 40), png = 3), # index 4 is the pixmap
+			],
+	"fonts": [gFont("Regular", 35),gFont("Regular", 25)],
+	"itemHeight": 64
+	}
+	</convert>
+
+<!-- info button -->
+</widget>
+<ePixmap position="1700,870" size="140,60" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/ElieSatPanel/images/info.png" zPosition="2" alphatest="blend" />
+
+</screen>"""
+
+	def __init__(self, session):
+		self.session = session
+		Screen.__init__(self, session)
+		self.setTitle(_("ElieSatPanel"))
+		self.iConsole = iConsole()
+		self.indexpos = None
+		self["shortcuts"] = NumberActionMap(["ShortcutActions", "WizardActions", "EPGSelectActions", "NumberActions"],
+		{
+			"ok": self.keyOK,
+			"cancel": self.exit,
+			"back": self.exit,
+			"red": self.keyRed,
+			"info": self.infoKey,
+			"green": self.keyGreen,
+			"yellow": self.keyYellow,
+			"blue": self.keyBlue,
+		})
+		self["key_red"] = StaticText(_("Config"))
+		self["key_green"] = StaticText(_("Softcam"))
+		self["key_yellow"] = StaticText(_("Tools"))
+		self["key_blue"] = StaticText(_("Install"))
+		self.list = []
+		self["menu"] = List(self.list)
+		self.mList()
+	def mList(self):
+		self.list = []
+		onepng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		twopng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		treepng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		fourpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		fivepng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		sixpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		sevenpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		eightpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		ninepng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		tenpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		elevenpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		twelvenpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		thirtenpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+
+		self.list.append((_("Ajpanel-14.0.0"), 1, _("إدارة الملفات و مجموعة كبيرة من الخدمات"), onepng))
+		self.list.append((_("Ciefpsettingspanel-1.9"), 2, _("بانل تسطيب اضافات"), twopng ))
+		self.list.append((_("Epanel-5.8r2"), 3, _("خدمات منوعة للصورة و المستخدم"), sixpng ))
+		self.list.append((_("Leviaddonsmanager-10.1r28"), 4, _("بانل تسطيب اضافات"), eightpng ))
+		self.list.append((_("Levimulticammanager-10.1r34"), 5, _("بانل تسطيب كامات و سكريبتات"), treepng))
+		self.list.append((_("Linuxsatpanel-2.7.0"), 6, _("بانل تسطيب اضافات"), sevenpng))
+		self.list.append((_("Eliesatpanel"), 7, _("اج بانل ايلي سات بانل"), fourpng))
+		self.list.append((_("Novaler-store-3.0r0"), 8, _("بانل تسطيب اضافات خاص باجهزة نوفالر"), ninepng))
+		self.list.append((_("Smartaddonspanel"), 9, _("بانل تسطيب اضافات "), elevenpng))
+		self.list.append((_("trial2"), 10, _("بانل تسطيب اضافات خاص باجهزة نوفالر"), twelvenpng))
+		self.list.append((_("s4aupdater"), 11, _("بانل تسطيب ملف قنوات بيكونات"), tenpng))
+		if self.indexpos != None:
+			self["menu"].setIndex(self.indexpos)
+		self["menu"].setList(self.list)
+		
+	def go(self, num = None):
+		if num is not None:
+			num -= 1
+			if not num < self["menu"].count():
+				return
+			self["menu"].setIndex(num)
+		item = self["menu"].getCurrent()[1]
+		self.select_item(item)
+		
+	def keyOK(self, item = None):
+		self.indexpos = self["menu"].getIndex()
+		if item == None:
+			item = self["menu"].getCurrent()[1]
+			self.select_item(item)
+
+	def select_item(self, item):
+		if item:
+			if item is 1:
+				self.session.open(Console, _("Installing package please wait..."), [
+            "clear >/dev/null 2>&1 && wget https://gitlab.com/eliesat/eliesatpanel/-/raw/main/eliesatpanel.sh -qO - | /bin/sh"
+        ])
+			elif item is 2:
+				self.session.open(PluginBrowser)
+			elif item is 3:
+				self.session.open(PluginBrowser)
+			elif item is 4:
+				self.session.open(PluginBrowser)
+			elif item is 5:
+				self.session.open(PluginBrowser)
+			elif item is 6:
+				self.session.open(PluginBrowser)
+			elif item is 7:
+				self.session.open(PluginBrowser)
+			elif item is 8:
+				self.session.open(PluginBrowser)
+			elif item is 9:
+				self.session.open(PluginBrowser)
+			elif item is 10:
+				self.session.open(PluginBrowser)
+			elif item is 11:
+				self.session.open(PluginBrowser)
+			else:
+				self.close(None)
+
+	def exit(self):
+		self.close()
+
+	def keyRed (self):
+
+		self.session.open(PluginBrowser)
+
+	def keyBlue (self):
+		self.session.open(PluginBrowser)
+				
+	def keyYellow (self):
+		self.session.open(PluginBrowser)
+		
+	def keyGreen (self):
+		self.session.open(PluginBrowser)
+	
+	def infoKey (self):
+		self.session.open(eliesatpanel)
+
+	def cancel(self):
+		self.close()
