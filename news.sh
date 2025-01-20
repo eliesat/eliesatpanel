0df842ee93108aa8d84e934f70956b17609a013c
eliesat <112230228+eliesat@users.noreply.github.com>
2025-01-20 09:33:36 +0200

    Add files via upload

---
+++ news.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+DATE="$(date +%a.%d.%b.%Y)"
+echo ">>>Eliesatpanel $DATE<<<"
+sleep 1
+echo ">>>zoukmosbeh-lebanon<<<"
+
+echo "> no new news "
+sleep 3
+
+echo
+echo ">>> Eliesat enjoy <<<"
+
