560c6373c6e97a4d3fff4803ad6fb5ff345d0b5f
eliesat <112230228+eliesat@users.noreply.github.com>
2025-01-21 16:17:18 +0200

    Add files via upload

--- menus/multiboot.py
+++ menus/multiboot.py
@@ -3,6 +3,7 @@
 
 from Plugins.Extensions.ElieSatPanel.menus.Console import Console
 import gettext
+from Components.Button import Button
 from Components.Language import language
 from Components.PluginComponent import plugins
 from Components.Sources.StaticText import StaticText
@@ -28,19 +29,24 @@ min = first_start = 0
 
 class multiboot(Screen):
 	skin = """
-<screen name="plugins" position="0,0" size="1920,1080" backgroundColor="transparent" flags="wfNoBorder" title="ElieSatPanel">
+<screen name="multiboot" position="0,0" size="1920,1080" backgroundColor="transparent" flags="wfNoBorder" title="ElieSatPanel">
 <ePixmap position="0,0" zPosition="-1" size="1920,1080" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/ElieSatPanel/images/bglist.png"/>
 
 <!-- title -->
-<eLabel text="MULTIBOOT" position="160,120" size="400,50" zPosition="1" font="Regular;45" halign="left" foregroundColor="white" backgroundColor="#ff2c2d2b" transparent="1" />
+<eLabel text="Multiboot" position="160,120" size="400,50" zPosition="1" font="Regular;40" halign="left" foregroundColor="white" backgroundColor="#ff2c2d2b" transparent="1" />
 <ePixmap position="55,125" size="180,47" zPosition="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/ElieSatPanel/images/1.png" alphatest="blend" />
 
+<!-- title1 -->
+<eLabel text="Multiboot" position="1510,700" size="400,50" zPosition="1" font="Regular;40" halign="left" foregroundColor="white" backgroundColor="#ff2c2d2b" transparent="1" />
+
+<!-- title2 -->
+<eLabel text="Select and press ok to install" position="1440,790" size="400,50" zPosition="1" font="Bold;27" halign="left" foregroundColor="white" backgroundColor="#ff2c2d2b" transparent="1" />
 
 <!-- minitv -->
 <widget source="session.VideoPicture" render="Pig" position="1320,120" size="550,400" zPosition="1" backgroundColor="#ff000000" />
 
 <!-- clock -->
-<widget source="global.CurrentTime" render="Label" position="1310,600" size="350,90" font="lsat; 75" noWrap="1" halign="center" valign="bottom" foregroundColor="#11ffffff" backgroundColor="#20000000" transparent="1" zPosition="2">
+<widget source="global.CurrentTime" render="Label" position="1290,600" size="350,90" font="lsat; 75" noWrap="1" halign="center" valign="bottom" foregroundColor="#11ffffff" backgroundColor="#20000000" transparent="1" zPosition="2">
 		<convert type="ClockToText">Default</convert>
 
 <!-- calender -->
@@ -64,7 +70,7 @@ class multiboot(Screen):
 <convert type="TemplatedMultiContent">
 	{"template": [
 		MultiContentEntryText(pos = (120, 10), size = (600, 45), font=0, flags = RT_HALIGN_LEFT, text = 0), # index 2 is the Menu Titel
-		MultiContentEntryText(pos = (700, 19), size = (600, 35), font=1, flags = RT_HALIGN_LEFT, text = 2), # index 3 is the Description
+		MultiContentEntryText(pos = (600, 19), size = (600, 35), font=1, flags = RT_HALIGN_LEFT, text = 2), # index 3 is the Description
 		MultiContentEntryPixmapAlphaTest(pos = (6, 10), size = (100, 40), png = 3), # index 4 is the pixmap
 			],
 	"fonts": [gFont("Regular", 35),gFont("Regular", 25)],
@@ -74,7 +80,7 @@ class multiboot(Screen):
 
 <!-- info button -->
 </widget>
-<ePixmap position="1700,870" size="140,60" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/ElieSatPanel/images/info.png" zPosition="2" alphatest="blend" />
+<ePixmap position="1530,870" size="140,60" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/ElieSatPanel/images/info.png" zPosition="2" alphatest="blend" />
 
 </screen>"""
 
@@ -84,51 +90,70 @@ class multiboot(Screen):
 		self.setTitle(_("ElieSatPanel"))
 		self.iConsole = iConsole()
 		self.indexpos = None
-		self["shortcuts"] = NumberActionMap(["ShortcutActions", "WizardActions", "EPGSelectActions", "NumberActions"],
+		self["shortcuts"] = NumberActionMap(["ShortcutActions", "WizardActions", "EPGSelectActions", "NumberActions" "ColorActions", "HotkeyActions"],
 		{
 			"ok": self.keyOK,
 			"cancel": self.exit,
 			"back": self.exit,
-			"red": self.keyRed,
+			"red": self.exit,
 			"info": self.infoKey,
 			"green": self.keyGreen,
-			"yellow": self.keyYellow,
-			"blue": self.keyBlue,
+			"yellow": self.keyOK,
+			"blue": self.restart,
 		})
-		self["key_red"] = StaticText(_("Config"))
-		self["key_green"] = StaticText(_("Softcam"))
-		self["key_yellow"] = StaticText(_("Tools"))
-		self["key_blue"] = StaticText(_("Install"))
+		self["key_red"] = StaticText(_("Close"))
+		self["key_green"] = StaticText(_("Browse"))
+		self["key_yellow"] = StaticText(_("Install"))
+		self["key_blue"] = StaticText(_("Restart E2"))
 		self.list = []
 		self["menu"] = List(self.list)
 		self.mList()
 	def mList(self):
 		self.list = []
-		onepng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
-		twopng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
-		treepng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
-		fourpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
-		fivepng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
-		sixpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
-		sevenpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
-		eightpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
-		ninepng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
-		tenpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
-		elevenpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
-		twelvenpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
-		thirtenpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		a = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		b = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		c = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		d = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		e = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		f = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		g = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		h = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		i = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		j = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		k = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		l = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		m = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		n = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		o = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		p = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		q = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		r = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		s = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		t = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
+		u = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/ElieSatPanel/images/1.png"))
 
-		self.list.append((_("Ajpanel-14.0.0"), 1, _("إدارة الملفات و مجموعة كبيرة من الخدمات"), onepng))
-		self.list.append((_("Ciefpsettingspanel-1.9"), 2, _("بانل تسطيب اضافات"), twopng ))
-		self.list.append((_("Epanel-5.8r2"), 3, _("خدمات منوعة للصورة و المستخدم"), sixpng ))
-		self.list.append((_("Leviaddonsmanager-10.1r28"), 4, _("بانل تسطيب اضافات"), eightpng ))
-		self.list.append((_("Levimulticammanager-10.1r34"), 5, _("بانل تسطيب كامات و سكريبتات"), treepng))
-		self.list.append((_("Linuxsatpanel-2.7.0"), 6, _("بانل تسطيب اضافات"), sevenpng))
-		self.list.append((_("Eliesatpanel"), 7, _("اج بانل ايلي سات بانل"), fourpng))
-		self.list.append((_("Novaler-store-3.0r0"), 8, _("بانل تسطيب اضافات خاص باجهزة نوفالر"), ninepng))
-		self.list.append((_("Smartaddonspanel"), 9, _("بانل تسطيب اضافات "), elevenpng))
-		self.list.append((_("trial2"), 10, _("بانل تسطيب اضافات خاص باجهزة نوفالر"), twelvenpng))
-		self.list.append((_("s4aupdater"), 11, _("بانل تسطيب ملف قنوات بيكونات"), tenpng))
+		self.list.append((_("Barryallen-12.91-r1"), 1, _("تسطيب صور خام على الذاكرة الخارجية"), a))
+		self.list.append((_("Egamiboot-10.6"), 2, _("تسطيب صور خام على الذاكرة الخارجية"), b))
+		self.list.append((_("Image-downloader-2.6"), 3, _("تنزيل صور خام على الذاكرة الخارجية py2"), c))
+		self.list.append((_("Meoboot-0.1"), 4, _("تسطيب صور على الذاكرة الخارجية"), d))
+		self.list.append((_("Multiboot-flashonline-7.1"), 5, _("تنزيل و تسطيب صور على الذاكرة الخارجية"), e))
+		self.list.append((_("Nfr4xboot-2.0"), 6, _("تسطيب صور على الذاكرة الخارجية"), f))
+		self.list.append((_("Neoboot-2.05"), 7, _("تسطيب صور على الذاكرة الخارجية"), g))
+		self.list.append((_("Neoboot-5.8"), 8, _("تسطيب صور على الذاكرة الخارجية"), h))
+		self.list.append((_("Neoboot-8.20"), 9, _("تسطيب صور على الذاكرة الخارجية"), i))
+		self.list.append((_("Neoboot-9.11"), 10, _("تسطيب صور على الذاكرة الخارجية"), j))
+		self.list.append((_("Neoboot-9.32"), 11, _("تسطيب صور على الذاكرة الخارجية mod linuxsat"), k))
+		self.list.append((_("Neoboot-9.36"), 12, _("تسطيب صور على الذاكرة الخارجية mod linuxsat"), l))
+		self.list.append((_("Neoboot-9.54"), 13, _("تسطيب صور على الذاكرة الخارجية mod linuxsat"), m))
+		self.list.append((_("Neoboot-9.58"), 14, _("تسطيب صور على الذاكرة الخارجية mod elsafty"), n))
+		self.list.append((_("Neoboot-9.60"), 15, _("تسطيب صور على الذاكرة الخارجية mod linuxsat"), o))
+		self.list.append((_("Neoboot-9.62"), 16, _("تسطيب صور على الذاكرة الخارجية mod linuxsat mips"), p))
+		self.list.append((_("Neoboot-9.65"), 17, _("تسطيب صور على الذاكرة الخارجية"), q))
+		self.list.append((_("Neoboot-duo4k-5.3"), 18, _("تسطيب صور على الذاكرة الخارجية"), r))
+		self.list.append((_("Neoboot-e2turk-3.9.1"), 19, _("تسطيب صور على الذاكرة الخارجية"), s))
+		self.list.append((_("Opdboot-3.1"), 20, _("تسطيب صور على الذاكرة الخارجية py3.11"), t))
+		self.list.append((_("Openmultiboot-1.0"), 21, _("تسطيب صور على الذاكرة الخارجية"), u))
+
 		if self.indexpos != None:
 			self["menu"].setIndex(self.indexpos)
 		self["menu"].setList(self.list)
@@ -152,33 +177,103 @@ class multiboot(Screen):
 		if item:
 			if item is 1:
 				self.session.open(Console, _("Installing package please wait..."), [
-            "clear >/dev/null 2>&1 && wget https://gitlab.com/eliesat/eliesatpanel/-/raw/main/eliesatpanel.sh -qO - | /bin/sh"
+            "wget --no-check-certificate https://gitlab.com/eliesat/extensions/-/raw/main/barryallen/barryallen.sh -qO - | /bin/sh"
         ])
 			elif item is 2:
-				self.session.open(PluginBrowser)
+				self.session.open(Console, _("Installing package please wait..."), [
+            "wget --no-check-certificate https://gitlab.com/eliesat/extensions/-/raw/main/egamiboot/egamiboot.sh -qO - | /bin/sh"
+        ])
 			elif item is 3:
-				self.session.open(PluginBrowser)
+				self.session.open(Console, _("Installing package please wait..."), [
+            "wget --no-check-certificate https://gitlab.com/eliesat/extensions/-/raw/main/image-downloader/image-downloader.sh -qO - | /bin/sh"
+        ])
 			elif item is 4:
-				self.session.open(PluginBrowser)
+				self.session.open(Console, _("Installing package please wait..."), [
+            "wget --no-check-certificate https://gitlab.com/eliesat/extensions/-/raw/main/meoboot/meoboot.sh -qO - | /bin/sh"
+        ])
 			elif item is 5:
-				self.session.open(PluginBrowser)
+				self.session.open(Console, _("Installing package please wait..."), [
+            "wget --no-check-certificate https://gitlab.com/eliesat/extensions/-/raw/main/multiboot-flashonline/multiboot-flashonline.sh -qO - | /bin/sh"
+        ])
 			elif item is 6:
-				self.session.open(PluginBrowser)
+				self.session.open(Console, _("Installing package please wait..."), [
+            "wget --no-check-certificate https://gitlab.com/eliesat/extensions/-/raw/main/nfr4xboot/nfr4xboot.sh -qO - | /bin/sh"
+        ])
 			elif item is 7:
-				self.session.open(PluginBrowser)
+				self.session.open(Console, _("Installing package please wait..."), [
+            "wget --no-check-certificate https://gitlab.com/eliesat/extensions/-/raw/main/neoboot/neoboot-2.05.sh -qO - | /bin/sh"
+        ])
 			elif item is 8:
-				self.session.open(PluginBrowser)
+				self.session.open(Console, _("Installing package please wait..."), [
+            "wget --no-check-certificate https://gitlab.com/eliesat/extensions/-/raw/main/neoboot/neoboot-5.8.sh -qO - | /bin/sh"
+        ])
 			elif item is 9:
-				self.session.open(PluginBrowser)
+				self.session.open(Console, _("Installing package please wait..."), [
+            "wget --no-check-certificate https://gitlab.com/eliesat/extensions/-/raw/main/neoboot/neoboot-8.20.sh -qO - | /bin/sh"
+        ])
 			elif item is 10:
-				self.session.open(PluginBrowser)
+				self.session.open(Console, _("Installing package please wait..."), [
+            "wget --no-check-certificate https://gitlab.com/eliesat/extensions/-/raw/main/neoboot/neoboot-9.11.sh -qO - | /bin/sh"
+        ])
 			elif item is 11:
-				self.session.open(PluginBrowser)
+				self.session.open(Console, _("Installing package please wait..."), [
+            "wget --no-check-certificate https://gitlab.com/eliesat/extensions/-/raw/main/neoboot/neoboot-9.32.sh -qO - | /bin/sh"
+        ])
+			elif item is 12:
+				self.session.open(Console, _("Installing package please wait..."), [
+            "wget --no-check-certificate https://gitlab.com/eliesat/extensions/-/raw/main/neoboot/neoboot-9.36.sh -qO - | /bin/sh"
+        ])
+			elif item is 13:
+				self.session.open(Console, _("Installing package please wait..."), [
+            "wget --no-check-certificate https://gitlab.com/eliesat/extensions/-/raw/main/neoboot/neoboot-9.54.sh -qO - | /bin/sh"
+        ])
+			elif item is 14:
+				self.session.open(Console, _("Installing package please wait..."), [
+            "wget --no-check-certificate https://gitlab.com/eliesat/extensions/-/raw/main/neoboot/neoboot-9.58-r3.sh -qO - | /bin/sh"
+        ])
+			elif item is 15:
+				self.session.open(Console, _("Installing package please wait..."), [
+            "wget --no-check-certificate https://gitlab.com/eliesat/extensions/-/raw/main/neoboot/neoboot-9.60.sh -qO - | /bin/sh"
+        ])
+			elif item is 16:
+				self.session.open(Console, _("Installing package please wait..."), [
+            "wget --no-check-certificate https://gitlab.com/eliesat/extensions/-/raw/main/neoboot/neoboot-9.62.sh -qO - | /bin/sh"
+        ])
+			elif item is 17:
+				self.session.open(Console, _("Installing package please wait..."), [
+            "wget --no-check-certificate https://gitlab.com/eliesat/extensions/-/raw/main/neoboot/neoboot-9.65.sh -qO - | /bin/sh"
+        ])
+			elif item is 18:
+				self.session.open(Console, _("Installing package please wait..."), [
+            "wget --no-check-certificate https://gitlab.com/eliesat/extensions/-/raw/main/neoboot/neoboot-duo4k.sh -qO - | /bin/sh"
+        ])
+			elif item is 19:
+				self.session.open(Console, _("Installing package please wait..."), [
+            "wget --no-check-certificate https://gitlab.com/eliesat/extensions/-/raw/main/neoboot/neoboot-e2turk.sh -qO - | /bin/sh"
+        ])
+			elif item is 20:
+				self.session.open(Console, _("Installing package please wait..."), [
+            "wget --no-check-certificate https://gitlab.com/eliesat/extensions/-/raw/main/opdboot/opdboot.sh -qO - | /bin/sh"
+        ])
+			elif item is 21:
+				self.session.open(Console, _("Installing package please wait..."), [
+            "wget --no-check-certificate https://gitlab.com/eliesat/extensions/-/raw/main/openmultiboot/openmultiboot.sh -qO - | /bin/sh"
+        ])
+
 			else:
 				self.close(None)
 
 	def exit(self):
 		self.close()
+
+	def keyRed (self):
+
+		self.session.open(PluginBrowser)
+
+	def restart (self):
+				self.session.open(Console, _("Restarting enigma2 please wait..."), [
+            "[ command -v dpkg &> /dev/null ] && systemctl restart enigma2 || killall -9 enigma2"
+        ])
 
 	def keyRed (self):
 
@@ -194,7 +289,9 @@ class multiboot(Screen):
 		self.session.open(PluginBrowser)
 	
 	def infoKey (self):
-		self.session.open(eliesatpanel)
+		self.session.open(Console, _("Please wait..."), [
+            "wget --no-check-certificate https://gitlab.com/eliesat/scripts/-/raw/main/check/_check-all.sh -qO - | /bin/sh"
+        ])
 
 	def cancel(self):
 		self.close()
