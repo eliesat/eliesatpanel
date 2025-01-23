4b283531a9759c415dde16074f4cbe8b22b65bbe
eliesat <112230228+eliesat@users.noreply.github.com>
2025-01-23 22:19:10 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 clear >/dev/null 2>&1
+# Check script url connectivity and install ajpanel_eliesatpanel
+#######################################
+if wget -q --method=HEAD https://github.com/eliesat/eliesatpanel/blob/main/installer.sh; then
+connection=ok
+else
+echo "> Server is down, try again later..."
+exit 1
+fi
+###########################################
+version='3.1'
+changelog='\panel update'
 
 # Functions
+###########################################
 print_message() {
     echo "[$(date +'%Y-%m-%d %H:%M:%S')] $1"
 }
@@ -14,7 +26,7 @@ sleep 2
 cleanup() {
     rm -rf /var/cache/opkg/* /var/lib/opkg/lists/* /run/opkg.lock $i >/dev/null 2>&1
 }
-
+###########################################
 image_version="/etc/image-version"
 box_type=$(head -n 1 /etc/hostname)
 distro_value=$(grep '^distro=' "$image_version" | awk -F '=' '{print $2}')
@@ -26,6 +38,43 @@ sleep 2
 print_message "> Python : $python_version"
 sleep 2
 
+# check libraries
+###########################################
+if [ -f /var/lib/dpkg/status ]; then
+   STATUS=/var/lib/dpkg/status
+   OSTYPE=DreamOs
+else
+   STATUS=/var/lib/opkg/status
+   OSTYPE=Dream
+fi
+echo ""
+if python --version 2>&1 | grep -q '^Python 3\.'; then
+	PYTHON=PY3
+	Packagesix=python3-six
+	Packagerequests=python3-requests
+else
+	PYTHON=PY2
+	Packagerequests=python-requests
+fi
+
+if [ $PYTHON = "PY3" ]; then
+	if grep -qs "Package: $Packagesix" cat $STATUS ; then
+		ok=ok
+	else
+		opkg update && opkg install python3-six >/dev/null 2>&1
+	fi
+fi
+if grep -qs "Package: $Packagerequests" cat $STATUS ; then
+	ok=ok
+else
+	if [ $OSTYPE = "DreamOs" ]; then
+		apt-get update && apt-get install python-requests -y >/dev/null 2>&1
+	elif [ $PYTHON = "PY3" ]; then
+		opkg update >/dev/null 2>&1 && opkg install python3-requests >/dev/null 2>&1
+	elif [ $PYTHON = "PY2" ]; then
+		opkg update >/dev/null 2>&1 && opkg install python-requests >/dev/null 2>&1
+	fi
+fi
 
 #configuration
 ###########################################
