12f2ed151c2c3b1809ba916bb9eec25a310a3470
eliesat <112230228+eliesat@users.noreply.github.com>
2025-01-24 11:48:42 +0200

    Add files via upload

--- news.sh
+++ news.sh
@@ -2,12 +2,13 @@
 
 DATE="$(date +%a.%d.%b.%Y)"
 echo ">>> Eliesatpanel $DATE (lebanon) <<<"
+echo
+echo
 sleep 1
-
 
 echo "> ElieSatPanel (trial-تجريبي) "
 sleep 3
-
 echo
 echo ">>> Eliesat enjoy <<<"
+echo
 
