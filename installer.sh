4cc5e3427b66209167394d42fa3f0d67face7141
eliesat <112230228+eliesat@users.noreply.github.com>
2025-01-24 13:41:20 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -5,8 +5,8 @@ clear >/dev/null 2>&1
 #configuration
 ###########################################
 plugin=main
-version='3.1'
-changelog='\panel update'
+version='2.1'
+changelog='auto update'
 url=https://github.com/eliesat/eliesatpanel/archive/main.tar.gz
 package=/tmp/$plugin.tar.gz
 rm -rf /tmp/$plugin.tar.gz >/dev/null 2>&1
