4386a0f00af60893334e7ac987b84f74160ca1bf
eliesat <112230228+eliesat@users.noreply.github.com>
2025-01-27 12:36:52 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -5,7 +5,7 @@ clear >/dev/null 2>&1
 #configuration
 ###########################################
 plugin=main
-version='2.27'
+version='2.28'
 changelog='27.01.2025'
 url=https://github.com/eliesat/eliesatpanel/archive/main.tar.gz
 package=/tmp/$plugin.tar.gz
--- mainmenu.py
+++ mainmenu.py
@@ -59,7 +59,7 @@ from types import *
 global min, first_start
 min = first_start = 0
 Panel = 'ElieSatPanel'
-Version = '2.27'
+Version = '2.28'
 installer = 'https://raw.githubusercontent.com/eliesat/eliesatpanel/main/installer.sh'
 scriptpath = "/usr/script/"
 
