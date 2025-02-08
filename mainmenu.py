5b81d5c9aaefd3e428fc07052b335cea6a98fb54
eliesat <112230228+eliesat@users.noreply.github.com>
2025-02-09 00:34:11 +0200

    Add files via upload

--- mainmenu.py
+++ mainmenu.py
@@ -59,7 +59,7 @@ from types import *
 global min, first_start
 min = first_start = 0
 Panel = 'ElieSatPanel'
-Version = '2.39'
+Version = '2.40'
 installer = 'https://raw.githubusercontent.com/eliesat/eliesatpanel/main/installer.sh'
 scriptpath = "/usr/script/"
 
