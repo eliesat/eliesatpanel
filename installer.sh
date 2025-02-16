97387093642fb09c814bb78b9d5004424d7d9c81
eliesat <112230228+eliesat@users.noreply.github.com>
2025-02-16 12:20:58 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -5,8 +5,8 @@ clear >/dev/null 2>&1
 #configuration
 ###########################################
 plugin=main
-version='2.47'
-changelog='1.16.02.2025'
+version='2.48'
+changelog='2.16.02.2025'
 url=https://github.com/eliesat/eliesatpanel/archive/main.tar.gz
 package=/tmp/$plugin.tar.gz
 rm -rf /tmp/$plugin.tar.gz >/dev/null 2>&1
@@ -117,8 +117,36 @@ print_message "> Eliesatpanel is installed successfully and up to date ..."
 echo
 sleep 2
 fi
+
+# Download and install scripts
+###########################################
+if [ ! -f /usr/script/Eliesat-Eliesatpanel.sh ] ; then
+
+plugin=main
+version='scripts'
+url=https://github.com/eliesat/scripts/archive/main.tar.gz
+package=/tmp/$plugin.tar.gz
+rm -rf /tmp/$plugin.tar.gz >/dev/null 2>&1
+
+wget -qO $package --no-check-certificate $url
+tar -xzf $package -C /tmp
+extract=$?
+rm -rf $package >/dev/null 2>&1
+
+if [ $extract -eq 0 ]; then
+    rm -rf /tmp/scripts-main/*.sh >/dev/null 2>&1
+    mkdir -p /usr/script >/dev/null 2>&1
+    cp -r '/tmp/scripts-main/usr' '/' >/dev/null 2>&1
+    rm -rf /tmp/scripts-main >/dev/null 2>&1
+
+print_message "> Eliesatscripts are installed successfully and up to date ..."
+sleep 3
+fi
+fi
+
 print_message "> End of process ..."
 sleep 3
+
 print_message "> Please Wait enigma2 restarting. ..."
 echo "-----------------------------------------------------------"
 sleep 3
