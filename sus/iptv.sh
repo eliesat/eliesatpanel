f3d6ccca2e08c1ff52cea080d42e2ef340bd0766
eliesat <112230228+eliesat@users.noreply.github.com>
2025-02-22 12:27:54 +0200

    Add files via upload

--- sus/iptv.sh
+++ sus/iptv.sh
@@ -2,13 +2,19 @@
 
 dir="/usr/lib/enigma2/python/Plugins/Extensions/ElieSatPanel/sus/iptv.txt"
 
+
 # Check for cmd cccam file
 ###########################################
-if [ -f $dir ]; then 
+if [  -s $dir ]; then 
 cmd=found
 sleep 1
 else
-echo "> cmd file not found"
+echo "> url= None"
+echo "> port= None"
+echo "> username= None"
+echo "> username= None"
+sleep 2
+echo "Write and save a new iptv user , then try again ..." 
 sleep 1
 exit 1 
 fi
@@ -17,20 +23,18 @@ fi
 ###########################################
 
 url=$(more $dir | head -n 1 |awk '{print $1}')
-echo $url
+echo "> url= $url"
 
 
 port=$(more $dir | head -n 1 |awk '{print $2}')
-echo $port
-
+echo "> port= $port"
 
 username=$(more $dir | head -n 1 |awk '{print $3}')
-echo $username
+echo "> username= $username"
 
 
 password=$(more $dir | head -n 1 |awk '{print $4}')
-echo $password
-
+echo "> password= $password"
 
 declare -A files=(
 ["bouquetmakerxtream"]="/etc/enigma2/bouquetmakerxtream/playlists.txt"
@@ -74,3 +78,4 @@ sleep 3
 fi
    fi
 done
+
