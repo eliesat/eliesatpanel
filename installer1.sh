729b524c5de9ffc1f0020aba06df1c5fed4850bf
eliesat <112230228+eliesat@users.noreply.github.com>
2025-09-20 01:16:33 +0300

    Add files via upload

---
+++ installer1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+clear >/dev/null 2>&1
+
+#configuration
+###########################################
+plugin=main
+version='3.04'
+changelog='1.25.08.2025'
+url=https://github.com/eliesat/eliesatpanel/archive/main.tar.gz
+package=/tmp/$plugin.tar.gz
+rm -rf /tmp/$plugin.tar.gz >/dev/null 2>&1
+
+# Check script url connectivity and install eliesatpanel
+###########################################
+if wget -q --method=HEAD https://github.com/eliesat/eliesatpanel/blob/main/installer.sh; then
+connection=ok
+else
+echo "> Server is down, try again later..."
+exit 1
+fi
+
+
+echo "hi this is a test"
+sleep 60
